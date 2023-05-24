afa56319d8f0ffefb8fd12f96a4a23267f130cf4
AditiGi <aditisingh20144@gmail.com>
2023-05-24 12:18:16 +0530

    lfklaop

---
+++ pages/about.js
@@ -0,0 +1,64 @@
+import Image from "next/image";
+
+const AboutPage = () => {
+  return (
+    <body>
+      <main style={{ background: "black" }}>
+        <section className="section-2">
+          <h1 className="about-heading">
+            I bring <span className="color-tag-1">websites to life</span> with
+            eye-catching designs and seamless functionality.
+          </h1>
+        </section>
+      </main>
+
+      <section className="section-3 working-about-description">
+        <div className="working-image-div">
+          {/* pic-1 class has been removed */}
+          <Image
+            src="/public/aditi-about.jpg"
+            alt="aditi's picture"
+            className="working-about-image"
+            width={800}
+            height={600}
+          />
+        </div>
+        <div className="working-about-section">
+          <h2 className="description-heading">
+            <span className="color-tag-1">Hey there,</span> I ama Aditi - an MBA
+            grad turned into front-end developer!
+          </h2>
+          <div className="flex-main">
+            <p className="description">
+              Do not let the degree fool you though - my real passion lies in
+              creating visually stunning interfaces that make users go wow When
+              I am not busy crafting code that would make even the Terminator
+              jealous, you will probably find me in front of my mirror, trying
+              to decide which outfit will make me look like the tech version of
+              Beyonce. I know, it is a tough job, but someone gotta do it!
+            </p>
+            <p className="description">
+              But all jokes aside, I take my work seriously and always strive to
+              create clean, efficient, and user-friendly interfaces. My
+              attention to detail and can-do attitude ensures that I am
+              constantly growing and learning in this ever-changing field. So,
+              if you are looking for a tech-savvy problem solver with a knack
+              for fashion and a good sense of humor, then look no further!
+            </p>
+            <p className="description">
+              Turning to tech was a steep path and being self-taught developer
+              was even steeper, but I am learning every day. Fueled by high
+              energy levels and boundless enthusiasm, I am easily inspired and
+              more than willing to follow my fascinations wherever they take me.
+              I am passionate, expressive being with a natural ability to
+              entertain and inspire. I am never satisfied to just come up with
+              ideas. Instead, I have an impulsive need to act on them.
+            </p>
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+};
+
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-// import { global } from "styled-jsx/css";
+//import { global } from "styled-jsx/css";
 import styles from "@/styles/Home.module.css";
+import AboutPage from "./AboutPage";
 
 const inter = Inter({ subsets: ["latin"] });
-console.log(styles);
+//console.log(styles);
 
 export default function Home() {
   return (
-    /* <div className={styles.container}>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="title" content="Aditi Singh" />
-        <meta name="description" content="I design and develop websites" />
-        <meta
-          name="keywords"
-          content="front-end developer, aditi singh, aditi, singh, UI-UX, coding, development, Javascript, css, portfolio, freelance, Jodhpur, front-end, programming"
-        />
-        <meta name="author" content="Aditi Singh(Alpha_Sierra)" />
-
-        <link
-          rel="icon"
-          href="./assets/logo-for-favicon-removebg-previe-copy.png"
-        />
-
-        
-        <nextscript
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2D2HKDTNKH"
-        ></nextscript>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                dataLayer.push(arguments);
-              }
-              gtag("js", new Date());
-              gtag("config", "G-2D2HKDTNKH");
-            `,
-          }}
-        /> 
-      </Head> */
-
     <main className={styles.main}>
       <section className={styles.section}>
         <div className={`${styles.introduction} ${styles["flex-intro"]}`}>
@@ -71,3 +35,13 @@ export default function Home() {
     // </div>
   );
 }
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js website!</h1>
+      <AboutPage />
+      {/* Add more components or content as needed */}
+    </div>
+  );
+};
